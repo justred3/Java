5dce201e068894a640bb00ad6d343342bd2df141
justred3 <jfhdajh@163.com>
2015-06-28 23:57:00 +0800

    day 44 jdk news

---
+++ src/day44_JDKnews/Direction3.java
@@ -0,0 +1,44 @@
+package day44_JDKnews;
+
+/*
+ * 通过JDK5提供的枚举来做枚举类
+ */
+public enum Direction3 {
+	FRONT("前") {
+		@Override
+		public void show() {
+			// TODO Auto-generated method stub
+			System.out.println("前");
+		}
+	}, BEHIND("后") {
+		@Override
+		public void show() {
+			// TODO Auto-generated method stub
+			System.out.println("后");
+		}
+	}, LEFT("左") {
+		@Override
+		public void show() {
+			// TODO Auto-generated method stub
+			System.out.println("左");
+		}
+	}, RIGHT("右") {
+		@Override
+		public void show() {
+			// TODO Auto-generated method stub
+			System.out.println("右");
+		}
+	};
+
+	private String name;
+
+	private Direction3(String name) {
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public abstract void show();
+}
